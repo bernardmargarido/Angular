636229b6b3cb4a8af2b24421f07d73c7610dff19
bernardmargarido <bernard.margarido@gmail.com>
2022-12-23 20:07:02 -0300

    Estudos Angular

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MyFirstComponent } from './myfirst.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponent
+  ],
+  imports: [
+    BrowserModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
