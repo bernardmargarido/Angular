c5a5384473c1d3bf295816d988aabc2dce1cd58e
bernardmargarido <bernard.margarido@gmail.com>
2023-12-06 20:30:02 -0300

    Estudos Angular

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,3 +1,4 @@
+import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from './../pensamentos';
 import { Component, Input } from '@angular/core';
 
@@ -7,17 +8,37 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent {
+
   @Input() pensamento: Pensamento = {
     id: 0,
     conteudo: 'Estudos Angular',
     autoria: 'Bernard',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
+
+  @Input() listaFavoritos: Pensamento[] = []
+
+  constructor(private service: PensamentoService){}
 
   larguraPensamento(): string {
     if(this.pensamento.conteudo.length > 256){
       return 'pensamento-g'
     }
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false){
+      return 'inativo'
+    }else{
+      return 'ativo'
+    }
+  }
+
+  atualizarFavoritos(){
+    this.service.mudarFavorito(this.pensamento).subscribe(() => {
+      this.listaFavoritos.splice(this.listaFavoritos.indexOf(this.pensamento),1)
+    })
   }
 }
